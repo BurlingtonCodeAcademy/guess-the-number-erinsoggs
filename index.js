5785367fd7f2f3facf2386240350772dde5a0820
Erin Soggs <erinasoggs@gmail.com>
2020-06-05 14:34:17 -0400

    made my program smarter, added number of guesses counter

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 let minNumber = 1;
-let maxNumber = 10;
+let maxNumber = 100;
 let computerGuess = randomNumber(minNumber, maxNumber);
+let guess = 0;
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
@@ -12,14 +13,15 @@ function ask(questionText) {
 }
 
 function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  // return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor((min + max) / 2);
 }
 
 start();
 
 async function start() {
   console.log(
-    "Let's play a game! Pick a number between 1 and 10 and I'll try to guess it!"
+    `Let's play a game! Pick a number between ${minNumber} and ${maxNumber} I'll try to guess it!`
   );
   let secretNumber = await ask(
     "Go ahead and pick a number by typing it into the console.\n(I promise I won't look!): "
@@ -32,6 +34,7 @@ async function start() {
     console.log("Your number is: " + secretNumber + ".");
     // The computer is going to try and guess the human's number using the randomNumber function.
     computerGuess = randomNumber(minNumber, maxNumber);
+    guess += 1;
     console.log(`I'm going to guess that your number is: ${computerGuess}`);
   }
 
@@ -42,7 +45,9 @@ async function start() {
     console.log("You're not speaking my language! Please restart the game!");
     process.exit();
   } else if (isGuessCorrect === "yes") {
-    console.log("Game over! I win! See you next time.");
+    console.log(
+      `I win! I guessed your number in ${guess} tries. See you next time.`
+    );
     process.exit();
   }
 
@@ -53,46 +58,35 @@ async function start() {
     );
     while (highLow.toLowerCase() !== "c")
       if (highLow.toLowerCase() === "h") {
-        if (minNumber + 1 < maxNumber) {
+        if (minNumber + 1 <= maxNumber) {
           minNumber = computerGuess + 1;
         }
-        console.log(minNumber);
         computerGuess = randomNumber(minNumber, maxNumber);
+        guess += 1;
         console.log(`My next guess is: ${computerGuess}`);
         highLow = await ask(
           "Is your number higher, lower, or did I guess correctly? Type h (higher), l (lower), or c (correct): "
         );
         if (highLow.toLowerCase() === "c") {
-          console.log("Game over! I win! See you next time.");
+          console.log(
+            `I win! I guessed your number in ${guess} tries. See you next time.`
+          );
           process.exit();
         }
       } else if (highLow.toLowerCase() === "l") {
         maxNumber = computerGuess - 1;
-        console.log(maxNumber);
         computerGuess = randomNumber(minNumber, maxNumber);
+        guess += 1;
         console.log(`My next guess is: ${computerGuess}`);
-        let highLowCorrect2 = await ask(
+        highLow = await ask(
           "Is your number higher, lower, or did I guess correctly? Type h (higher), l (lower), or c (correct): "
         );
-        if (highLowCorrect2.toLowerCase() === "c") {
-          console.log("Game over! I win! See you next time.");
+        if (highLow.toLowerCase() === "c") {
+          console.log(
+            `I win! I guessed your number in ${guess} tries. See you next time.`
+          );
           process.exit();
         }
       }
   }
 }
-
-// } else if (highLowCorrect.toLowerCase() === "l") {
-//   maxNumber = computerGuess - 1;
-//   computerGuess = randomNumber(minNumber, maxNumber);
-//   console.log(`My next guess is: ${computerGuess}`);
-// }
-
-//   } else if (highLow.toLowerCase() === "l") {
-//     maxNumber = computerGuess - 1;
-//     computerGuess = randomNumber(minNumber, maxNumber);
-//     console.log(`My next guess is: ${computerGuess}`);
-//   } else if (highLow.toLowerCase() === "c") {
-//     console.log("Game over! I win! See you next time.");
-//     process.exit();
-//
