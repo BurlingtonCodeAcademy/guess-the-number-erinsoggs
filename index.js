76907286a22be5d896ff7f67fc297b97eee389b2
Erin Soggs <erinasoggs@gmail.com>
2020-06-06 11:01:46 -0400

    implemented cheat blocks and simplified my code because of this

--- index.js
+++ index.js
@@ -37,14 +37,7 @@ async function start() {
     guess += 1;
     console.log(`I'm going to guess that your number is: ${computerGuess}`);
   }
-
-  let isGuessCorrect = await ask(
-    "Did I guess correctly? Type yes or no, pleeeease!: "
-  );
-  if (isGuessCorrect.toLowerCase() !== "yes" && isGuessCorrect !== "no") {
-    console.log("You're not speaking my language! Please restart the game!");
-    process.exit();
-  } else if (isGuessCorrect === "yes") {
+  if (secretNumber === computerGuess) {
     console.log(
       `I win, because winners always guess your number in ${guess} tries! I'm going to go eat a celebratory pint (or three) of Cherry Garcia now. See you next time!`
     );
@@ -52,65 +45,31 @@ async function start() {
   }
 
   // This will loop if the answer isn't initially "yes" or until the computer guesses correctly (c)
-  while (isGuessCorrect === "no" && computerGuess <= maxNumber) {
+  while (secretNumber !== computerGuess) {
     let highLow = await ask(
       "Is your number higher or lower? Type h (higher) or l (lower): "
     );
     if (highLow.toLowerCase() !== "h" && highLow.toLowerCase() !== "l") {
       console.log("You're not speaking my language! Please restart the game!");
       process.exit();
-    }
-    while (highLow.toLowerCase() !== "c")
-      if (highLow.toLowerCase() === "h") {
-        if (minNumber + 1 <= maxNumber) {
-          minNumber = computerGuess + 1;
-        }
-        computerGuess = randomNumber(minNumber, maxNumber);
-        guess += 1;
-        console.log(`My next guess is: ${computerGuess}`);
-        highLow = await ask(
-          "Is your number higher, lower, or did I guess correctly? Type h (higher), l (lower), or c (correct): "
-        );
-        if (
-          highLow.toLowerCase() !== "h" &&
-          highLow.toLowerCase() !== "l" &&
-          highLow.toLowerCase() !== "c"
-        ) {
-          console.log(
-            "You're not speaking my language! Please restart the game!"
-          );
-          process.exit();
-        }
-        if (highLow.toLowerCase() === "c") {
-          console.log(
-            `I win, because winners always guess your number in ${guess} tries! I'm going to go eat a celebratory pint (or three) of Cherry Garcia now. See you next time!`
-          );
-          process.exit();
-        }
-      } else if (highLow.toLowerCase() === "l") {
-        maxNumber = computerGuess - 1;
-        computerGuess = randomNumber(minNumber, maxNumber);
-        guess += 1;
-        console.log(`My next guess is: ${computerGuess}`);
-        highLow = await ask(
-          "Is your number higher, lower, or did I guess correctly? Type h (higher), l (lower), or c (correct): "
-        );
-        if (
-          highLow.toLowerCase() !== "h" &&
-          highLow.toLowerCase() !== "l" &&
-          highLow.toLowerCase() !== "c"
-        ) {
-          console.log(
-            "You're not speaking my language! Please restart the game!"
-          );
-          process.exit();
-        }
-        if (highLow.toLowerCase() === "c") {
-          console.log(
-            `I win, because winners always guess your number in ${guess} tries! I'm going to go eat a celebratory pint (or three) of Cherry Garcia now. See you next time!`
-          );
-          process.exit();
-        }
+    } else if (highLow.toLowerCase() === "h") {
+      if (minNumber + 1 <= maxNumber) {
+        minNumber = computerGuess + 1;
       }
+      computerGuess = randomNumber(minNumber, maxNumber);
+      guess += 1;
+      console.log(`My next guess is: ${computerGuess}`);
+    } else if (highLow.toLowerCase() === "l") {
+      maxNumber = computerGuess - 1;
+      computerGuess = randomNumber(minNumber, maxNumber);
+      guess += 1;
+      console.log(`My next guess is: ${computerGuess}`);
+    }
+    if (secretNumber === computerGuess) {
+      console.log(
+        `I win, because winners always guess your number in ${guess} tries! I'm going to go eat a celebratory pint (or three) of Cherry Garcia now. See you next time!`
+      );
+      process.exit();
+    }
   }
 }
